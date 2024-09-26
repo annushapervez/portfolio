bd34f00d352ba34f41b864df818c031d4d636113
annushapervez <annushapervez7@gmail.com>
2024-09-26 16:29:44 -0400

    fixed responsiveness of home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Script from 'next/script';
 import Image from 'next/image';
 import SideNav from '../components/SideNav'; // Ensure the path is correct
 import "../styles/nav.css"; // Ensure the path is correct
-import "../styles/termynal.css"; // Ensure the path is correct
 import animationData from '../public/animation.json';
 import animationData1 from '../public/animation2.json';
 import ProjectCarousel from '../components/ProjectCarousel';
@@ -29,95 +28,84 @@ export default function Home() {
 
       <main className="main-content">
         <section id="section0" className="section">
-    
-          <div className="image-and-text">
-            <div className="image-container">
-              <Image
-                src="/iphone2.png" // Ensure this path is correct
-                alt="iPhone"
-                width={350} // Match the width of the image
-                height={300} // Match the height of the image
-                priority
-              />
-
-              <div className="bubbles">
-                <a href="https://www.linkedin.com/in/annusha-pervez/" target="_blank" rel="noopener noreferrer" className="bubble bubble1">
-                  <Image
-                    src="/linkedin2.png" // Ensure this path is correct
-                    alt="LinkedIn Bubble"
-                    width={261}
-                    height={26}
-                    className="bubble-image"
-                  />
-                </a>
-
-                <a href="https://github.com/annushapervez" target="_blank" rel="noopener noreferrer" className="bubble bubble2">
-                  <Image
-                    src="/github2.png" // Ensure this path is correct
-                    alt="GitHub Bubble"
-                    width={261}
-                    height={26}
-                    className="bubble-image"
-                  />
-                </a>
-
-                <a href="mailto:annushapervez7@gmail.com" className="bubble bubble3">
-                  <Image
-                    src="/email2.png" // Ensure this path is correct
-                    alt="Email Bubble"
-                    width={261}
-                    height={26}
-                    className="bubble-image"
-                  />
-                </a>
+          <div className="content-columns">
+              {/* iPhone Wrapper */}
+              <div className="iphone-wrapper">
+              <div className="image-container">
+                <Image
+                  src="/iphone2.png"
+                  alt="iPhone"
+                  width={350}
+                  height={300}
+                  priority
+                />
+                <div className="bubbles">
+                  <a href="https://www.linkedin.com/in/annusha-pervez/" target="_blank" rel="noopener noreferrer" className="bubble bubble1">
+                    <Image
+                      src="/linkedin2.png"
+                      alt="LinkedIn Bubble"
+                      width={261}
+                      height={26}
+                      className="bubble-image"
+                    />
+                  </a>
+                  <a href="https://github.com/annushapervez" target="_blank" rel="noopener noreferrer" className="bubble bubble2">
+                    <Image
+                      src="/github2.png"
+                      alt="GitHub Bubble"
+                      width={261}
+                      height={26}
+                      className="bubble-image"
+                    />
+                  </a>
+                  <a href="mailto:annushapervez7@gmail.com" className="bubble bubble3">
+                    <Image
+                      src="/email2.png"
+                      alt="Email Bubble"
+                      width={261}
+                      height={26}
+                      className="bubble-image"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
-            
-            <div className="typing-container">
-              <span id="sentence" className="sentence"></span>
-              <span className="input-cursor"></span>    
-            </div>
-            {/* Recruitment status and blinking light */}
+            {/* Terminal Column */}
+            <div className="terminal-container">
+              <div className="typing-container">
+                <span id="sentence" className="sentence"></span>
+                <span className="input-cursor"></span>
+              </div>
+
+            {/* Recruitment Column */}
             <div className="recruitment-container">
-              {/* JSON animation */}
               <div className="json-animation">
-              <Lottie
-  animationData={animationData} // Use the imported variable here
-  loop
-  autoplay
-  style={{ width: '30px', height: '30px'}} // Adjust size as needed
-/>
+                <Lottie animationData={animationData} loop autoplay style={{ width: '30px', height: '30px' }} />
               </div>
-
-              {/* Recruitment Status Text */}
               <div className="recruitment-status">
                 <h2>Recruitment Status</h2>
                 <p>Currently open to new opportunities.</p>
               </div>
+            </div>
+              <div id="termynal" data-termynal data-ty-typedelay="40" data-ty-linedelay="700">
+                {/* Terminal Content */}
+                <span data-ty="input">ls Languages</span>
+                <span data-ty>Python, C, Scala, PHP, Go, JavaScript, TypeScript, CSS, HTML, OCaml, Mips Assembly</span>
+                <span data-ty></span>
 
-             {/* Terminal block goes here */}
-             <div id="termynal" data-termynal data-ty-typedelay="40" data-ty-linedelay="700">
-              {/* Computer Languages */}
-              <span data-ty="input">ls Languages</span>
-              <span data-ty>Python, C, Scala, PHP, Go, JavaScript, TypeScript, CSS, HTML, OCaml, Mips Assembly</span>
-              <span data-ty></span>
+                <span data-ty="input">ls Frameworks</span>
+                <span data-ty>Flask, TensorFlow, React, Next.js, Unreal Engine 5</span>
+                <span data-ty></span>
 
-              {/* Frameworks */}
-              <span data-ty="input">ls Frameworks</span>
-              <span data-ty>Flask, TensorFlow, React, Next.js, Unreal Engine 5</span>
-              <span data-ty></span>
+                <span data-ty="input">ls Databases</span>
+                <span data-ty>MongoDB, SQL</span>
+                <span data-ty></span>
 
-              {/* Databases */}
-              <span data-ty="input">ls Databases</span>
-              <span data-ty>MongoDB, SQL</span>
-              <span data-ty></span>
-
-              {/* Tools */}
-              <span data-ty="input">ls Tools</span>
-              <span data-ty>Docker, GitHub, Git</span>
-              <span data-ty></span>
-        </div>
-        </div>
+                <span data-ty="input">ls Tools</span>
+                <span data-ty>Docker, GitHub, Git</span>
+                <span data-ty></span>
+              </div>
+            </div>
           </div>
         </section>
 
