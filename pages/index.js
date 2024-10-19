2fe046e5ca91fec90d4eb7b34aee941a9915c7d3
annushapervez <annushapervez7@gmail.com>
2024-10-19 14:39:37 -0400

    deploy error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 "use client"; // Add this at the top of your file
-
 import Head from 'next/head';
 import Script from 'next/script';
 import Image from 'next/image';
@@ -96,8 +95,8 @@ export default function Home() {
   ];
 
   const aboutMeContent = {
-    introduction: " I'm a passionate software engineer based in New York City with a unique blend of expertise in computer science and psychology. I graduated from the University at Buffalo with a Bachelor of Science in Computer Science and a Bachelor of Arts in Psychology in May 2024. My academic journey has equipped me with a solid foundation in data structures, algorithms, web development, machine learning, and software quality practices.",
-    problemSolving: " I thrive on tackling complex problems and developing innovative solutions. I enjoy finding creative approaches to challenges, whether it's optimizing algorithms, designing intuitive user experiences, or implementing efficient systems. Every problem presents a new puzzle, and I am always eager to explore unconventional solutions that push boundaries and deliver meaningful results.",
+    introduction: " I am a passionate software engineer based in New York City with a unique blend of expertise in computer science and psychology. I graduated from the University at Buffalo with a Bachelor of Science in Computer Science and a Bachelor of Arts in Psychology in May 2024. My academic journey has equipped me with a solid foundation in data structures, algorithms, web development, machine learning, and software quality practices.",
+    problemSolving: " I thrive on tackling complex problems and developing innovative solutions. I enjoy finding creative approaches to challenges, whether it is optimizing algorithms, designing intuitive user experiences, or implementing efficient systems. Every problem presents a new puzzle, and I am always eager to explore unconventional solutions that push boundaries and deliver meaningful results.",
     seeking: "I am actively seeking a software engineering position where I can apply my skills in web development, distributed systems, and software engineering to contribute to innovative projects. I’m excited to collaborate with a dynamic team and work in an environment that fosters growth, creativity, and impactful problem-solving."
   };
 
